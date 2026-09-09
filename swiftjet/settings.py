75149002d24a5251322d580f6e2c32f22293ea86
Laura Neema <laurajuma52@gmail.com>
2026-09-09 08:06:20 +0300

    Fix bug

--- swiftjet/settings.py
+++ swiftjet/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
-SECRET_KEY = " 2g$4w8)i%&9=u+0o%u3&4keye=vrh(s=i9^q9=p8)ou5-01!yn"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "swiftjet-2jys.onrender.com"]
 
 
 
