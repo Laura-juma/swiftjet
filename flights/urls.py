1affd3509a2a162b2fdee061d90977b86cd62bf7
Laura Neema <laurajuma52@gmail.com>
2026-09-23 20:57:52 +0300

    Remove temporary views

--- flights/urls.py
+++ flights/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, BookFlight, BookingReview, search_flights, PaymentView, mpesa_callback, payment_status, database_test, setup_production
+from .views import home, BookFlight, BookingReview, search_flights, PaymentView, mpesa_callback, payment_status
 
 urlpatterns = [
     path("", home, name="home"),
@@ -9,15 +9,6 @@ urlpatterns = [
     path("payment/<int:booking_id>/", PaymentView, name="payment"),
     path("mpesa/callback/", mpesa_callback, name="mpesa_callback"),
     path("payment-status/<int:booking_id>/", payment_status, name="payment_status"),
-
-
-
-    path("database-test/", database_test, name="database_test"),
-    path(
-    "setup-production/",
-    setup_production,
-    name="setup_production"
-),
 
 
 ]
